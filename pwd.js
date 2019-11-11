2031df573e721ea0cb319bbf45ae5c42819cd6da
Dylan Horgan <dyhorgan@gmail.com>
2019-11-11 14:32:35 -0500

    Here it is

---
+++ pwd.js
@@ -0,0 +1,12 @@
+
+
+module.exports = function(){
+    process.stdout.write('prompt > ');
+    process.stdin.on('data', (data) => {
+      
+        const pwd = process.cwd()
+        process.stdout.write(pwd);
+        process.stdout.write('\nprompt > ');
+    
+    });
+}
